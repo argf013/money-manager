7fc08b0d0da27a516d077aa2003d3dce55bde5a7
argf013 <alghi344@gmail.com>
2025-05-17 21:16:03 +0700

    fix: sw.js error

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -29,6 +28,11 @@ export default defineConfig({
       },
       devOptions: {
         enabled: true,
+      },
+      strategies: 'injectManifest',
+      injectManifest: {
+        swSrc: './src/service-worker.ts',
+        swDest: 'service-worker.js',
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
